8b42a4981ae78aa5cd66b017e419f05fe2c229e1
Colin MacDonald <colin.r.macdonald@gmail.com>
2019-11-25 20:07:09 -0500

    Implement naive `count` utility

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::collections::HashMap;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut counts: HashMap<String, usize> = HashMap::new();
+
+    loop {
+        let mut input = String::new();
+        match io::stdin().read_line(&mut input) {
+            Ok(0) => break,
+            Ok(_) => {
+                let count = counts.entry(input.to_string()).or_insert(0);
+                *count += 1;
+            }
+            Err(_) => break,
+        }
+    }
+    for (key, value) in counts.iter() {
+        println!("{:7} {}", value, key.trim());
+    }
 }
